a31fa4c690a82dc951f0d551345018de07010e59
Nahid Afridy <85471971+NAHID-AFRIDY@users.noreply.github.com>
2022-08-15 14:14:53 +0600

    Update nahid.py

--- nahid.py
+++ nahid.py
@@ -4,11 +4,11 @@ os.system('git pull')
 import requests
 bit = platform.architecture()[0]
 if bit == '64bit':
-    from pni import menu
+    from pni import pni
     menu()
 elif bit == '32bit':
-    from pni import menu
-    menu()
+    from pni import pni
+    pni()
 
 
 
